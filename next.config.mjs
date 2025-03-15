0f6c7975b56f8a5181c6fe8d8457ef799b69e394
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2025-03-15 15:24:39 +0100

    added the python

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,46 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-}
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	experimental: {
+		webpackBuildWorker: true,
+		parallelServerBuildTraces: true,
+		parallelServerCompiles: true,
+	},
+	rewrites: async () => {
+		return [
+			{
+				source: "/api/py/:path*",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/:path*"
+						: "/api/",
+			},
+			{
+				source: "/docs",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/docs"
+						: "/api/py/docs",
+			},
+			{
+				source: "/openapi.json",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/openapi.json"
+						: "/api/py/openapi.json",
+			},
+		];
+	},
+};
 
 mergeConfig(nextConfig, userConfig)
 
